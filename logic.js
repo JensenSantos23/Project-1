b9b09358afdd50631858532a848d3885d2615358
Andy <andrew.marshall1@outlook.com>
2018-11-28 21:28:56 -0700

    updates

--- logic.js
+++ logic.js
@@ -6,12 +6,23 @@ $("#submit-button").on("click", function(event) {
     // Capture User Inputs and store them into variables
     var name = $("#name-input").val();
     var email = $("#email-input").val();
-    var team = $("#team-input").val();
+    var team = $("#team-input").val().replace(/\s+/g, '-');
     
+
     // Console log each of the user inputs to confirm we are receiving them
     console.log(name);
     console.log(email);
     console.log(team);
+ 
+      var queryURL = "https://api.seatgeek.com/2/events?client_id=MTQxMzIwNjV8MTU0MzQ1OTIxMC4yNg&performers.slug=" + team;
+      $.ajax({
+       url: queryURL
+      }).then(function(response) {
+       console.log(response);
+      console.log(response.events[0].title);
+      
+
+      });
 
     // Output all of the new information into the relevant HTML sections
     $("#name-display").text(name);
@@ -32,15 +43,21 @@ $("#submit-button").on("click", function(event) {
   $("#email-display").text(localStorage.getItem("email"));
   $("#team-display").text(localStorage.getItem("team"));
 
+});
 
+// //var queryURL = "http://api.sportradar.us/nba/trial/v5/en/seasons/2018/REG/standings.json?api_key=jfjvkawctrfhsskhejx55ykt";
+// var queryURL = "http://api.sportradar.us/nba/trial/v5/en/games/22ca891e-3589-40d1-b9ca-31196c83b883/boxscore.json?api_key=jfjvkawctrfhsskhejx55ykt"
+// $.ajax({
+//   url: queryURL
+// }).then(function(response) {
+//   console.log(response);
+// })
 
-});   
-
-//var queryURL = "http://api.sportradar.us/nba/trial/v5/en/seasons/2018/REG/standings.json?api_key=jfjvkawctrfhsskhejx55ykt";
-var queryURL = "http://api.sportradar.us/nba/trial/v5/en/games/22ca891e-3589-40d1-b9ca-31196c83b883/boxscore.json?api_key=jfjvkawctrfhsskhejx55ykt"
-$.ajax({
-  url: queryURL
-}).then(function(response) {
-  console.log(response);
-})
-
+function searchFavoriteTeam(team) {
+  var queryURL = "https://api.seatgeek.com/2/events?client_id=MTQxMzIwNjV8MTU0MzQ1OTIxMC4yNg&performers.slug=" + team;
+  $.ajax({
+   url: queryURL
+  }).then(function(response) {
+   console.log(response);
+  })
+}
